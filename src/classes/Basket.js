50a396a4884e0aeddbe9d892b19bc3f6eab9abba
Jenocke <jenocke@gmail.com>
2023-03-02 17:30:27 +0100

    added the basket size property, changed the constructor to be able to create a larger basket and the check if capacity exceeded when trying to add an item

--- src/classes/Basket.js
+++ src/classes/Basket.js
@@ -1,10 +1,22 @@
 
 class Basket{
-    constructor(){
+    constructor(size = 5){
         this.itemsArray = [];
+        this.size = size;
+    }
+
+    GetCurrentCapacity(){
+        let capacity = 0;
+        this.itemsArray.forEach(element => {
+            capacity+=element.quantity;
+        });
+        return capacity;
     }
 
     AddBagel(bagelToAdd){
+        let capacity = this.GetCurrentCapacity();
+        if(capacity == this.size)
+            return false;
         const itemIndex = this.itemsArray.findIndex((item) => item.bagel.sku === bagelToAdd.sku)
         if(itemIndex == -1)
         {
@@ -16,27 +28,27 @@ class Basket{
         }
         return true;
     }
-
+    
+    RemoveBagel(bagel){
+        let itemIndex;
+        let test = typeof(bagel);
+        if(test === "string")
+        {
+            itemIndex = this.itemsArray.findIndex((item) => item.bagel.sku === bagel)
+        }
+        else
+        itemIndex = this.itemsArray.findIndex((item) => item.bagel.sku === bagel.sku)
+        if(this.itemsArray[itemIndex].quantity <=1)
+        this.itemsArray.splice(itemIndex, 1);
+        else
+        this.itemsArray[itemIndex].quantity --;
+    }
+    
     GetItems(){
         return this.itemsArray;
     }
 
-    RemoveBagel(bagel){
-        let itemIndex;
-        let test = typeof(bagel);
-        console.log(typeof(bagel));
-        if(test === "string")
-        {
-            console.log("test - string");
-            itemIndex = this.itemsArray.findIndex((item) => item.bagel.sku === bagel)
-        }
-        else
-            itemIndex = this.itemsArray.findIndex((item) => item.bagel.sku === bagel.sku)
-        if(this.itemsArray[itemIndex].quantity <=1)
-            this.itemsArray.splice(itemIndex, 1);
-        else
-            this.itemsArray[itemIndex].quantity --;
-    }
+    
 }
 
 module.exports = Basket;
